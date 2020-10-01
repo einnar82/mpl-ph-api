09c70b5a3eba31251b2dbbd5e2c04b700618a313
yash-93 <yashdeepbachhas93@gmail.com>
2020-10-01 18:43:03 +0530

    API endpoint to get all team names

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import "dotenv/config";
 
 import { parsePlayerStatistitcs } from "./parsers/getPlayersStatistics";
+import { getTeams } from "./parsers/getAllTeams";
 
 const app = express();
 
@@ -21,11 +22,17 @@ app.get("/players/stats", async (req, res) => {
   }
 });
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "Hello ExpressJS!",
-  });
+app.get("/allTeams", async (req, res) => {
+  try{
+    const allTeams = await getTeams();
+    res.json({
+      allTeams,
+    });
+  }catch(err){
+    res.status(500).send(err);
+  }
 });
+
 
 app.listen(4000, () => {
   console.log(`app is listening to port 4000`);
